fa2b05ab6236c5ad6927696fe00b9f2ea575b580
Jelena Jankovic <jelena.cvetkovic.bg@gmail.com>
2021-10-23 01:06:42 +0200

    AoC 2020 Day 23 - second part

--- 2020/day23.py
+++ 2020/day23.py
@@ -3,272 +3,265 @@ inputs_ = '469217538'
 
 
 def decrease_value(value, min_v, max_v):
-    res = value - 1
-    if res < min_v:
-        res = max_v
-    return res
+    return value - 1 if value > min_v else max_v
 
 
 def cyndex(index, lenc):
     return index % lenc
 
 
-def final(circle):
-    one_index = circle.index(1)
-    res = []
-    for i in range(len(circle) - 1):
-        res.append(circle[cyndex(one_index + i + 1, len(circle))])
-    return ''.join(str(x) for x in res)
-
-
-def mini_cups():
+def mini_cups(rng=100):
+    """ No real optimization required"""
     circle = [int(c) for c in inputs_]
     min_cup = min(circle)
     max_cup = max(circle)
     lenc = len(circle)
     current_index = 0
-    destination = -1
-    for _ in range(100):
-        out = []
+    for _ in range(rng):
         current = circle[current_index]
-        cp_circle = circle.copy()
 
+        out = []
         for i in range(3):
             out.append(
-                cp_circle.pop(cyndex(cp_circle.index(current) + 1, len(cp_circle)))
+                circle.pop(cyndex(circle.index(current) + 1, len(circle)))
             )
+
         destination = decrease_value(current, min_cup, max_cup)
         while destination in out:
             destination = decrease_value(destination, min_cup, max_cup)
-        insert_index = cp_circle.index(destination)
-        for o in out:
-            insert_index = cyndex(insert_index + 1, len(cp_circle))
-            cp_circle.insert(insert_index, o)
-        circle = cp_circle
-        current_index = circle.index(current)
-        current_index = cyndex(current_index + 1, len(circle))
 
-        # print(out, current, destination)
-    print(final(circle))
+        insert_index = circle.index(destination)
+
+        for o in out:
+            insert_index = cyndex(insert_index + 1, len(circle))
+            circle.insert(insert_index, o)
+
+        current_index = circle.index(current)
+        current_index = cyndex(current_index + 1, lenc)
+
+    one_index = circle.index(1)
+    res = [
+        circle[cyndex(one_index + i + 1, lenc)] for i in range(len(circle) - 1)
+    ]
+    return ''.join(str(x) for x in res)
+
+
+def min_c_distannce(index1, index2, lenc):
+    pos_dst = cyndex(index2 - index1, lenc)
+    neg_dst = cyndex(index1 - index2, lenc)
+    if pos_dst <= neg_dst:
+        return pos_dst
+    return -neg_dst
 
 
 def slide(circle, first_empty, delta, insert, fill):
     lenc = len(circle)
     insert = cyndex(insert - delta + 1, lenc)
-    from_fill = False
-    for i in range(lenc):
-        index = cyndex(first_empty + i, lenc)
-        if index == insert:
-            from_fill = True
-        if not from_fill:
-            circle[index] = circle[cyndex(index + delta, lenc)]
-        else:
-            circle[index] = fill.pop(0)
-        if len(fill) == 0:
-            break
+    dst = min_c_distannce(first_empty, insert, lenc)
+    if dst >= 0:
+        from_fill = False
+        for i in range(lenc):
+            index = cyndex(first_empty + i, lenc)
+            if index == insert:
+                from_fill = True
+            if not from_fill:
+                circle[index] = circle[cyndex(index + delta, lenc)]
+            else:
+                circle[index] = fill[0]
+                circle[cyndex(index + 1, lenc)] = fill[1]
+                circle[cyndex(index + 2, lenc)] = fill[2]
+                break
+    else:
+        from_fill = False
+        for i in range(lenc):
+            index = cyndex(first_empty + delta - 1 - i, lenc)
+            if index == cyndex(insert + delta, lenc):
+                from_fill = True
+            if not from_fill:
+                circle[index] = circle[cyndex(index - delta, lenc)]
+            else:
+                circle[index] = fill[0]
+                circle[cyndex(index + 1, lenc)] = fill[1]
+                circle[cyndex(index + 2, lenc)] = fill[2]
+                break
 
-import utils
 
+def maxi_cups_slide(length=1000000, rng=10000000):
+    """
+    Do not do pop and insert, but slide whole sections
 
-@utils.print_duration
-def maxi_cups_slide():
+    T=42.205 (10000 iterations)
+    """
     circle = [int(c) for c in inputs_]
     min_cup = min(circle)
     max_cup = max(circle)
-    circle.extend([i for i in range(max_cup + 1, 1000001)])
+    circle.extend([i for i in range(max_cup + 1, length + 1)])
     max_cup = max(circle)
     lenc = len(circle)
     current_index = 0
-    destination = -1
-    # for xx in range(100):
-    for xx in range(100):
-        if xx % 100 == 0:
-            print(xx)
-        out = []
+    for _ in range(rng):
         current = circle[current_index]
-        # cp_circle = circle.copy()
-        # print(circle)
-        for i in range(3):
-            out.append(
-                circle[cyndex(current_index + i + 1, lenc)]
-            )
+
+        out = [circle[cyndex(current_index + i + 1, lenc)]
+               for i in range(3)]
+
         destination = decrease_value(current, min_cup, max_cup)
         while destination in out:
             destination = decrease_value(destination, min_cup, max_cup)
+
         insert_index = circle.index(destination)
-        # print(out, current, destination)
 
         slide(circle, cyndex(current_index + 1, lenc), 3, insert_index, out)
 
+        current_index = circle.index(current)
         current_index = cyndex(current_index + 1, lenc)
-    # print(final(circle))
+
+    one_at = circle.index(1)
+    return circle[cyndex(one_at + 1, lenc)] * circle[cyndex(one_at + 2, lenc)]
 
 
-@utils.print_duration
-def maxi_cups():
+def maxi_cups(length=1000000, rng=10000000):
+    """
+    Mini without helper functions.
+    Added insert_index optimization
+
+    T=32.057 (10000 iterations)
+    """
     circle = [int(c) for c in inputs_]
     min_cup = min(circle)
     max_cup = max(circle)
-    circle.extend([i for i in range(max_cup + 1, 1000001)])
+    circle.extend([i for i in range(max_cup + 1, length + 1)])
     max_cup = max(circle)
     lenc = len(circle)
     current_index = 0
-    destination = -1
-    # for xx in range(100):
-    for xx in range(10000):
-        if xx % 1000 == 0:
-            print(xx)
-        out = []
+    insert_index = 0
+    for _ in range(rng):
         current = circle[current_index]
-        cp_circle = circle.copy()
-        # print(cp_circle)
-        for i in range(3):
-            out.append(
-                cp_circle.pop((cp_circle.index(current) + 1) % len(cp_circle))
-            )
-        destination = decrease_value(current, min_cup, max_cup)
+
+        out = [circle[(current_index + 1 + i) % lenc] for i in range(3)]
+
+        for _ in out:
+            circle.pop((current_index + 1) % lenc)
+
+        destination = current - 1 if current > min_cup else max_cup
         while destination in out:
-            destination = decrease_value(destination, min_cup, max_cup)
-        insert_index = cp_circle.index(destination)
+            destination = current - 1 if current > min_cup else max_cup
+
+        if destination != circle[insert_index]:
+            insert_index = circle.index(destination)
+
         for o in out:
-            insert_index = (insert_index + 1) % len(cp_circle)
-            cp_circle.insert(insert_index, o)
-        circle = cp_circle
-        current_index = circle.index(current)
-        current_index = (current_index + 1) % lenc
+            insert_index = (insert_index + 1) % len(circle)
+            circle.insert(insert_index, o)
 
-        # print(out, current, destination)
-    one_index = circle.index(1)
-    res = []
-    for i in range(lenc - 1):
-        res.append(circle[(one_index + i + 1)% lenc])
-        if len(res) == 2:
-            break
-    print(res[0] * res[1])
+        current_index = (circle.index(current) + 1) % lenc
 
-@utils.print_duration
-def maxi_cups_slicing():
-    circle = [int(c) for c in inputs_]
+    one_at = circle.index(1)
+    return circle[cyndex(one_at + 1, lenc)] * circle[cyndex(one_at + 2, lenc)]
+
+
+def maxi_cups_deque(length=1000000, rng=10000000):
+    """
+    Using deque to optimize insertion/deletion.
+    Reduce inner functions.
+    Added insert_index finding optimization.
+
+    T=18.973 (for 10000 iterations)
+    """
+    from collections import deque
+
+    circle = deque(int(c) for c in inputs_)
     min_cup = min(circle)
     max_cup = max(circle)
-    circle.extend([i for i in range(max_cup + 1, 1000001)])
+    circle.extend([i for i in range(max_cup + 1, length + 1)])
     max_cup = max(circle)
     lenc = len(circle)
     current_index = 0
-    destination = -1
-    for xx in range(10000):
-    # for xx in range(1000):
-        if xx % 1000 == 0:
-            print(xx)
-        out = []
+    insert_index = 0
+    for _ in range(rng):
         current = circle[current_index]
-        # cp_circle = circle.copy()
-        # print(xx + 1, circle)
-        out = [
-            circle[(current_index + 1) % lenc],
-            circle[(current_index + 2) % lenc],
-            circle[(current_index + 3) % lenc]
-        ]
 
-        # for i in range(3):
-        #     out.append(
-        #         cp_circle.pop((cp_circle.index(current) + 1) % len(cp_circle))
-        #     )
+        out = [circle[(current_index + 1 + i) % lenc] for i in range(3)]
+        for o in out:
+            circle.remove(o)
+
+        destination = current - 1 if current > min_cup else max_cup
+        while destination in out:
+            destination = current - 1 if current > min_cup else max_cup
+
+        if destination != circle[insert_index]:
+            insert_index = circle.index(destination)
+
+        for o in out:
+            insert_index = (insert_index + 1) % len(circle)
+            circle.insert(insert_index, o)
+
+        current_index = (circle.index(current) + 1) % lenc
+
+    one_at = circle.index(1)
+    return circle[cyndex(one_at + 1, lenc)] * circle[cyndex(one_at + 2, lenc)]
+
+
+def go_linked(circle, indexes, start, steps):
+    index = indexes[start]
+    nexts = [index]
+    for _ in range(steps):
+        index = indexes[index]
+        nexts.append(index)
+    values = [circle[i] for i in nexts[:-1]]
+    return values, nexts
+
+
+def maxi_cups_linked(length=1000000, rng=10000000):
+    """
+    Use linked list instead of an array.
+    Added insert index finding optimization.
+
+    T=0.334 (10000 iterations)
+    """
+    circle = [int(c) for c in inputs_]
+    min_cup = min(circle)
+    max_cup = max(circle)
+    circle.extend([i for i in range(max_cup + 1, length + 1)])
+    max_cup = max(circle)
+    lenc = len(circle)
+    current_index = 0
+    next_index = [
+        cyndex(i + 1, lenc) for i in range(lenc)
+    ]
+    for _ in range(rng):
+        current = circle[current_index]
+
+        out, indexes = go_linked(circle, next_index, current_index, 3)
+
         destination = decrease_value(current, min_cup, max_cup)
         while destination in out:
             destination = decrease_value(destination, min_cup, max_cup)
 
-        insert_index = (circle.index(destination) + 1) % lenc
-        curr_index_1 = (current_index + 1) % lenc
-        curr_index_4 = (current_index + 4) % lenc
-        if insert_index > curr_index_4 > curr_index_1:
-            circle = circle[: curr_index_1] + \
-                circle[curr_index_4: insert_index] + out + \
-                circle[insert_index:]
-        elif curr_index_4 > curr_index_1 > insert_index:
-            circle = circle[: insert_index] + out +\
-                circle[insert_index: curr_index_1] + \
-                circle[curr_index_4 :]
-        elif curr_index_1 > insert_index > curr_index_4:
-            circle = circle[curr_index_4:insert_index] + out + \
-                circle[insert_index:curr_index_1]
+        if destination == circle[cyndex(destination - 1, len(circle))]:
+            insert_index = destination - 1
         else:
-            circle = circle
-            print('ERROR', curr_index_1, curr_index_4, insert_index)
+            insert_index = circle.index(destination)
 
-        # for o in out:
-        #     insert_index = (insert_index + 1) % len(cp_circle)
-        #     cp_circle.insert(insert_index, o)
-        #     cp_circle.
-        # circle = cp_circle
-        current_index = circle.index(current)
-        current_index = (current_index + 1) % lenc
+        destination_new_next = next_index[current_index]
+        end_index_new_next = next_index[insert_index]
+        current_index_new_next = indexes[-1]
+        next_index[current_index] = current_index_new_next
+        next_index[insert_index] = destination_new_next
+        next_index[indexes[-2]] = end_index_new_next
 
-        # print(out, current, destination)
-    # print(final(circle))
+        current_index = next_index[current_index]
+
     one_index = circle.index(1)
-    res = []
-    for i in range(lenc - 1):
-        res.append(circle[(one_index + i + 1)% lenc])
-        if len(res) == 2:
-            break
-    print(res[0] * res[1])
+    first_after_one_index = next_index[one_index]
+    second_after_one_index = next_index[first_after_one_index]
+    return circle[first_after_one_index] * circle[second_after_one_index]
 
 
 if __name__ == '__main__':
-    mini_cups()
-    maxi_cups_slicing()
-    # maxi_cups()
+    print(mini_cups())
 
-# -- move 1 --
-# cups: (3) 8  9  1  2  5  4  6  7
-# pick up: 8, 9, 1
-# destination: 2
-#
-# -- move 2 --
-# cups:  3 (2) 8  9  1  5  4  6  7
-# pick up: 8, 9, 1
-# destination: 7
-#
-# -- move 3 --
-# cups:  3  2 (5) 4  6  7  8  9  1
-# pick up: 4, 6, 7
-# destination: 3
-#
-# -- move 4 --
-# cups:  7  2  5 (8) 9  1  3  4  6
-# pick up: 9, 1, 3
-# destination: 7
-#
-# -- move 5 --
-# cups:  3  2  5  8 (4) 6  7  9  1
-# pick up: 6, 7, 9
-# destination: 3
-#
-# -- move 6 --
-# cups:  9  2  5  8  4 (1) 3  6  7
-# pick up: 3, 6, 7
-# destination: 9
-#
-# -- move 7 --
-# cups:  7  2  5  8  4  1 (9) 3  6
-# pick up: 3, 6, 7
-# destination: 8
-#
-# -- move 8 --
-# cups:  8  3  6  7  4  1  9 (2) 5
-# pick up: 5, 8, 3
-# destination: 1
-#
-# -- move 9 --
-# cups:  7  4  1  5  8  3  9  2 (6)
-# pick up: 7, 4, 1
-# destination: 5
-#
-# -- move 10 --
-# cups: (5) 7  4  1  8  3  9  2  6
-# pick up: 7, 4, 1
-# destination: 3
-#
-# -- final --
-# cups:  5 (8) 3  7  4  1  9  2  6
+    # print(maxi_cups_deque(rng=10000))
+    # print(maxi_cups(rng=10000))
+    # print(maxi_cups_slide(rng=10000))
+    print(maxi_cups_linked())

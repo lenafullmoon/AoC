5d35822942db490c804fb16149ca59737695cceb
Jelena Jankovic <jelena.cvetkovic.bg@gmail.com>
2021-12-09 23:19:49 +0100

    AoC 2021 Day 06 - condense

--- 2021/day06.py
+++ 2021/day06.py
@@ -3,24 +3,21 @@ if __name__ == '__main__':
         inputs_ = fp.read()
 
     solution_1_initial = [int(i) for i in inputs_.split(',')]
-    for day in range(80):
-        zero_count = 0
+    for _ in range(80):
+        zero_count = solution_1_initial.count(0)
         for fish_index in range(len(solution_1_initial)):
             if solution_1_initial[fish_index] == 0:
-                zero_count += 1
-                solution_1_initial[fish_index] = 6
-            else:
-                solution_1_initial[fish_index] -= 1
+                solution_1_initial[fish_index] = 7
+            solution_1_initial[fish_index] -= 1
         solution_1_initial.extend([8] * zero_count)
     print(len(solution_1_initial))
 
     solution_2_initial = [int(i) for i in inputs_.split(',')]
-    ages_to_count = {k: solution_2_initial.count(k) for k in range(9)}
-    for day in range(256):
-        zero_count = ages_to_count[0]
-        for age in range(6):
-            ages_to_count[age] = ages_to_count[age + 1]
-        ages_to_count[6] = ages_to_count[7] + zero_count
-        ages_to_count[7] = ages_to_count[8]
-        ages_to_count[8] = zero_count
-    print(sum(ages_to_count.values()))
+    age_to_count = {a: solution_2_initial.count(a) for a in range(9)}
+    for _ in range(256):
+        zero_count = age_to_count[0]
+        for age in range(8):
+            age_to_count[age] = age_to_count[age + 1]
+        age_to_count[6] += zero_count
+        age_to_count[8] = zero_count
+    print(sum(age_to_count.values()))

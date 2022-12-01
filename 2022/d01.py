3f6a5aaad592474616e8ad920e57a4e8f6aa0998
Jelena Jankovic <jelena.cvetkovic.bg@gmail.com>
2022-12-01 20:44:03 +0100

    AoC 2022 Day 01

---
+++ 2022/d01.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+
+    with open('src/d01.txt') as fp:
+        inputs_ = fp.read()
+
+    calories_carried = [0]
+    for row in inputs_.split('\n'):
+        if row:
+            calories_carried[-1] += int(row)
+        else:
+            calories_carried.append(0)
+
+    calories_carried.sort(reverse=True)
+    print(calories_carried[0])
+    print(sum(calories_carried[:3]))

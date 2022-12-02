5e8ecaa98cdc7f838ad989b53a47e2a4047e86de
Jelena Jankovic <jelena.cvetkovic.bg@gmail.com>
2022-12-02 18:50:32 +0100

    AoC 2022 Day 02 - pretty

--- 2022/d02.py
+++ 2022/d02.py
@@ -1,61 +1,29 @@
-inputs_ = '''A Y
-B X
-C Z'''
-loses = {'B X', 'A Z', 'C Y'}
-draws = {'A X', 'B Y', 'C Z'}
+def win_for(move):
+    return 'S' if move == 'R' else 'R' if move == 'P' else 'P'
 
-to_win = {
-    'A': 'B',
-    'B': 'C',
-    'C': 'A'
-}
 
-to_lose = {
-    'A': 'C',
-    'B': 'A',
-    'C': 'B'
-}
+def decipher(c):
+    return 'R' if c in 'XA' else 'P' if c in 'YB' else 'S'
+
+
+def score(opponent, me):
+    points = 6 if win_for(me) == opponent else 3 if me == opponent else 0
+    return points + (1 if me == 'R' else 2 if me == 'P' else 3)
+
+
+def from_outcome(opponent, outcome):
+    return (opponent if outcome == 'Y' else  # y == draw
+            win_for(opponent) if outcome == 'X' else  # x == lose
+            'RSP'.replace(opponent, '').replace(win_for(opponent), ''))
 
 
 if __name__ == '__main__':
     with open('src/d02.txt') as fp:
         inputs_ = fp.read()
-    points = 0
+    points1, points2 = 0, 0
     for row in inputs_.splitlines(keepends=False):
-        if row in loses:
-            points += 0
-        elif row in draws:
-            points += 3
-        else:
-            points += 6
-        if row.endswith('X'):
-            points += 1
-        elif row.endswith('Y'):
-            points += 2
-        else:
-            points += 3
-    print(points)
-
-# X - lose, Y - draw, Z - win
-    points = 0
-    for row in inputs_.splitlines(keepends=False):
-        opponent, outcome = row.split()
-        my_choice = None
-        if outcome == 'X':
-            points += 0
-            my_choice = to_lose[opponent]
-        elif outcome == 'Y':
-            points += 3
-            my_choice = opponent
-        else:
-            points += 6
-            my_choice = to_win[opponent]
-        if my_choice == 'A':
-            points += 1
-        elif my_choice == 'B':
-            points += 2
-        else:
-            points += 3
-
-    print(points)
-
+        opponent_move = decipher(row[0])
+        points1 += score(opponent_move, decipher(row[2]))
+        points2 += score(opponent_move, from_outcome(opponent_move, row[2]))
+    print(points1)
+    print(points2)

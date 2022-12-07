746f51d3b4e63dd811d9c793197f7c21fbeeba17
Jelena Jankovic <jelena.cvetkovic.bg@gmail.com>
2022-12-07 23:02:29 +0100

    AoC 2022 Day 07 - condensed

--- 2022/d07.py
+++ 2022/d07.py
@@ -1,48 +1,36 @@
+def find_current_dir(current_dir, current_path):
+    for p in current_path:
+        current_dir = current_dir[p]
+    return current_dir
+
+
 if __name__ == '__main__':
     with open('inputs/d07.txt') as fp:
         inputs_ = fp.read()
-    tree = {}
+
+    tree = {'/': {}}
+    dir_sizes = {'/': 0}
+    current_position = tree['/']
     path = []
-    dir_sizes = {}
-    current_position = None
     for line in inputs_.splitlines(keepends=False):
         line = line.split()
         if line[0] == '$':
             if line[1] == 'cd' and line[2] != '..':
                 path.append(line[2])
-                current_position = tree
-                for p in path:
-                    current_position.setdefault(p, {})
-                    current_position = current_position[p]
+                current_position[line[2]] = {}
+                current_position = find_current_dir(tree, path)
                 dir_sizes['/'.join(path)] = 0
             elif line[1] == 'cd' and line[2] == '..':
                 path.pop()
-                current_position = tree
-                for p in path:
-                    current_position.setdefault(p, {})
-                    current_position = current_position[p]
-            elif line[1] == 'ls':
-                pass
-        else:
-            if line[0] == 'dir':
-                pass
-                # current_position[line[1]] = {}
-            else:
-                current_position[line[1]] = line[0]
-                parents = path.copy()
-                while parents:
-                    dir_sizes['/'.join(parents)] += int(line[0])
-                    parents.pop()
+                current_position = find_current_dir(tree, path)
+        elif line[0] != 'dir':
+            parents = path.copy()
+            while parents:
+                dir_sizes['/'.join(parents)] += int(line[0])
+                parents.pop()
 
     print(sum(size for size in dir_sizes.values() if size <= 100000))
 
-    max_space = 70000000
-    target_free_space = 30000000
-    free_space = max_space - dir_sizes['/']
-    missing_space = target_free_space - free_space
-    print(free_space, missing_space)
-    size_to_delete = max_space
-    for dir_, size in dir_sizes.items():
-        if missing_space < size < size_to_delete:
-            size_to_delete = size
-    print(size_to_delete)
+    MAX_SPACE, TARGET_SPACE = 70000000, 30000000
+    missing_space = TARGET_SPACE - (MAX_SPACE - dir_sizes['/'])
+    print(min(size for size in dir_sizes.values() if size > missing_space))

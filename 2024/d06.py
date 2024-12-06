c85078c7b55fabae32ad4c97df8c7c13f9a6e9ac
Jelena Jankovic <jelena.cvetkovic.bg@gmail.com>
2024-12-06 19:57:19 +0100

    AoC 2024 Day 06 - first part, prettier solution

--- 2024/d06.py
+++ 2024/d06.py
@@ -10,39 +10,70 @@ inputs_ = '''....#.....
 #.........
 ......#...'''
 
+
+class Position:
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+
+    def __add__(self, other):
+        return Position(self.i + other.i, self.j + other.j)
+
+    def __str__(self):
+        return f'({self.i},{self.j})'
+
+
+class Direction:
+    UP = Position(-1, 0)
+    RIGHT = Position(0, 1)
+    DOWN = Position(1, 0)
+    LEFT = Position(0, -1)
+
+    order = [UP, RIGHT, DOWN, LEFT]
+
+    @classmethod
+    def next(cls, direction):
+        return cls.order[(cls.order.index(direction) + 1) % len(cls.order)]
+
+
+class Territory:
+    def __init__(self, m):
+        self.map = m
+
+    def __getitem__(self, position):
+        return self.map[position.i][position.j]
+
+    def __setitem__(self, position, value):
+        self.map[position.i][position.j] = value
+
+    def in_bounds(self, position):
+        return (
+            0 <= position.i < len(self.map)
+            and 0 <= position.j < len(self.map[0])
+        )
+
+    def starting_position(self):
+        for i in range(len(self.map)):
+            for j in range(len(self.map[0])):
+                if self.map[i][j] == '^':
+                    return Position(i, j)
+
+
 if __name__ == '__main__':
     with open('inputs/d06.txt') as fp:
         inputs_ = fp.read()
-    territory = [[row[i] for i in range(len(row))]
-                 for row in inputs_.splitlines(keepends=False)]
+    territory = Territory([[row[i] for i in range(len(row))]
+                          for row in inputs_.splitlines(keepends=False)])
 
-    guard_start_position = (-1, -1)
-    for i in range(len(territory)):
-        for j in range(len(territory[0])):
-            if territory[i][j] == '^':
-                guard_start_position = (i, j)
-                break
-    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    guard_direction_index = -1
     guard_path_length = 1
-    gp = guard_start_position
-    while True:
-        guard_direction_index = (guard_direction_index + 1) % len(directions)
-        gd = directions[guard_direction_index]
-        while territory[gp[0] + gd[0]][gp[1] + gd[1]] != '#':
-            gp = (gp[0] + gd[0], gp[1] + gd[1])
-            guard_path_length += 1 if territory[gp[0]][gp[1]] == '.' else 0
-            territory[gp[0]][gp[1]] = 'X'
-
-            if (0 > gp[0] + gd[0] or gp[0] + gd[0] >= len(territory)
-                    or 0 > gp[1] + gd[1] or gp[1] + gd[1] >= len(
-                        territory[0])):
-                break
-
-        if (0 > gp[0] + gd[0] or gp[0] + gd[0] >= len(territory)
-                or 0 > gp[1] + gd[1] or gp[1] + gd[1] >= len(territory[0])):
-            break
+    guard_position = territory.starting_position()
+    guard_direction = Direction.UP
+    while territory.in_bounds(guard_position + guard_direction):
+        if territory[guard_position + guard_direction] == '#':
+            guard_direction = Direction.next(guard_direction)
+            continue
+        guard_position += guard_direction
+        guard_path_length += 1 if territory[guard_position] == '.' else 0
+        territory[guard_position] = 'X'
 
     print(guard_path_length)
-
-

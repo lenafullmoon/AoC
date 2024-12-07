0a1c5fe1fd1571aa15acbb5dfb36aac06d69afbd
Jelena Jankovic <jelena.cvetkovic.bg@gmail.com>
2024-12-08 00:10:05 +0100

    AoC 2024 Day 06 - second part fix for quick solution

--- 2024/d06.py
+++ 2024/d06.py
@@ -1,14 +1,10 @@
 # correct: 1304 (part 2)
-inputs_ = '''....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#...'''
+inputs_ = '''.........#......................#.......
+..#.......#.....#..#.....#.#.......#....
+......#.................O.........#....#
+#..............#..#..#...........#...##.
+.........#..............................
+......^..#....#................#........'''
 
 
 class Position:
@@ -23,7 +19,7 @@ class Position:
         return Position(self.i - other.i, self.j - other.j)
 
     def __repr__(self):
-        return f'({self.i},{self.j})'
+        return f'({self.i}, {self.j})'
 
     def __eq__(self, other):
         return self.i == other.i and self.j == other.j
@@ -102,21 +98,21 @@ class Territory:
 
 
 if __name__ == '__main__':
-    with open('inputs/d06.txt') as fp:
-        inputs_ = fp.read()
+    # with open('inputs/d06.txt') as fp:
+    #     inputs_ = fp.read()
     territory = Territory([[row[i] for i in range(len(row))]
                           for row in inputs_.splitlines(keepends=False)])
     guard_path_length = 1
     guard = Guard(territory.starting_position())
     path = [guard.position]
-    possible_obstacles = set()
+    possible_obstacles = []
     while territory.in_bounds(guard.next()):
         if territory[guard.next()] == '#':
             guard.turn()
-            continue
-        guard.step()
-        guard_path_length += 1 if guard.position not in path else 0
-        path.append(guard.position)
+        else:
+            guard.step()
+            guard_path_length += 1 if guard.position not in path else 0
+            path.append(guard.position)
 
         # if I turn right now, do I come by to a path in the same direction
         if guard.next() in possible_obstacles:
@@ -125,24 +121,26 @@ if __name__ == '__main__':
             continue
         if guard.next() == territory.starting_position():
             continue
+        if guard.next() in path[:-1]:
+            continue
+        if (guard.next().i, guard.next().j) in [(2, 26), (2, 24), (2, 18)]:
+            print(guard.next())
         swap = territory[guard.next()]
         territory[guard.next()] = '#'
         print(len(possible_obstacles), guard_path_length, guard.next())
         possible_guard = guard.copy()
-        possible_guard.turn()
-        possible_loop = {possible_guard.copy()}
+        possible_loop = [possible_guard.copy()]
         while territory.in_bounds(possible_guard.next()):
             # print(possible_guard, territory[possible_guard.position])
             if territory[possible_guard.next()] == '#':
-                possible_loop.add(possible_guard.copy())
                 possible_guard.turn()
-                continue
-            possible_guard.step()
+            else:
+                possible_guard.step()
             if possible_guard in possible_loop:
-                possible_obstacles.add(guard.next())
+                possible_obstacles.append(guard.next())
                 # print('obsticle found')
                 break
-            possible_loop.add(possible_guard.copy())
+            possible_loop.append(possible_guard.copy())
         # else:
         #     print('out of bounds')
         # print(possible_loop)
@@ -150,7 +148,9 @@ if __name__ == '__main__':
 
     print(guard_path_length)
     print(len(possible_obstacles))
+    print(possible_obstacles)
 
+    obs = []
     obs_count = 0
     for i in range(len(territory.map)):
         for j in range(len(territory.map[0])):
@@ -158,7 +158,7 @@ if __name__ == '__main__':
                 continue
             if Position(i, j) not in path:
                 continue
-            print(i, j)
+            # print(i, j)
             tterr = territory
             swap = tterr.map[i][j]
             tterr.map[i][j] = '#'
@@ -175,7 +175,9 @@ if __name__ == '__main__':
                     break
                 potential_loop.append(guard.copy())
             if looped:
+                obs.append((i, j))
                 print('found', i, j)
                 obs_count += 1
             tterr.map[i][j] = swap
     print(obs_count)
+    print(obs)
